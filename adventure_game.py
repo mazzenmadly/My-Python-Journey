fb2559c9621e5d692cca89df76567b77017df103
mazzenmadly <mazenadly2008@gmail.com>
2026-07-17 23:53:24 +0300

    adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,70 +1,60 @@
 import random
+import time
+import sys
 
-list_of_weapons = ["shotgun", "pistol", " sniper"]
+list_of_weapons = ["shotgun", "pistol", "sniper"]
 ur_weapon = random.choice(list_of_weapons)
 list_of_sec_weapon = ["knife", "katana", "sword"]
 ur_secweapon = random.choice(list_of_sec_weapon)
-import time
-
 score = 0
-import sys
 
+def print_pause():
+    time.sleep(2)
 
 # the introduction
 def intro():
-
     print("Hello adventurer")
     print_pause()
-    print(
-        "You find yourself standing in an open jungle, filled with trees and",
-        " animals.",
-    )
+    print("You find yourself standing in an open jungle, filled with trees and animals.")
     print_pause()
-    print(
-        "Rumor has it that a huge creature is somewhere around here, and has",
-        "been terrifying the nearby village.",
-    )
+    print("Rumor has it that a huge creature is somewhere around here, and has been terrifying the nearby village.")
     print_pause()
     print("In front of you is an old house.")
     print_pause()
     print("To your right is a dark cave.")
     print_pause()
-    print(
-        f"In your hand you hold your a {ur_weapon} and"
-        "a {ur_secweapon} (but the {ur_secweapon} is not very effective) ."
-    )
+    # حرف الـ f رجع هنا عشان يطبع الأسلحة صح
+    print(f"In your hand you hold a {ur_weapon} and a {ur_secweapon} (but the {ur_secweapon} is not very effective).")
     print_pause()
+    print("enter 1 to explore the dark cave.")
+    print_pause()
+    print("enter 2 to go to the old house.")
+    print_pause()
+    print("what would you like to do?")
 
-    print("enter 1 to explore the dark cave .")
-    print_pause()
-    print("enter 2 to go to  the old house.")
-    print_pause()
-    print("what would you like to do ?")
-
-#replay the game 
+# replay the game 
 def try_again():
-    print("would you like to play again ?")
+    print("would you like to play again?")
     print_pause()
-    print("enter 1 to accept .")
+    print("enter 1 to accept.")
     print_pause()
     print("enter 2 to refuse.")
     restart = input()
+    
     if restart == "1":
         game()
     elif restart == "2":
-        print("thanks for playing our game ")
+        print("thanks for playing our game")
         # sys.exit()
-
-    elif not restart == ["1", "2"]:
+    # التعديل الصح للشرط
+    elif restart not in ["1", "2"]:
         print("please enter a valid answer or the game will close")
         restart = input()
         if restart == "1":
             game()
-
         elif restart == "2":
-            print("thanks for playing our game ")
+            print("thanks for playing our game")
             # sys.exit()
-
 
 def cave():
     print("you went to the cave")
@@ -73,68 +63,57 @@ def cave():
     print_pause()
     print("you don't have enough armor")
     print_pause()
-    print("you can't defeat the group ")
+    print("you can't defeat the group")
     print_pause()
-    print("you have killed by them")
+    print("you have been killed by them")
     print_pause()
     print("you lose")
     print_pause()
-    print(f"you score ={score}")
+    print(f"your score = {score}")
     print_pause()
     try_again()
-
-
-def print_pause():
-    time.sleep(2)
-
 
 def house():
     print("you enterd the house")
     print_pause()
     print("there is an old woman")
     print_pause()
-    print("the woman offerd to help you" " and give you the equipment that you need")
+    print("the woman offerd to help you and give you the equipment that you need")
     print_pause()
-    print("enter 1 to accept .")
+    print("enter 1 to accept.")
     print_pause()
     print("enter 2 to refuse.")
     print_pause()
-    print("what would you like to do ?")
+    print("what would you like to do?")
 
     woman = input()
     if woman == "1":
-        print("the woman gave  you the equipment that you need")
+        print("the woman gave you the equipment that you need")
         print_pause()
         print("the woman told you the place of the creature")
         print_pause()
-        print(
-            "the woman told you that the"
-              " creature is hiding in the village's river")
+        print("the woman told you that the creature is hiding in the village's river")
         print_pause()
         print("now you are going to the river to kill the creature")
         print_pause()
-        print("now you  aproached to riverbank")
-        print("you saw the creature ")
+        print("now you aproached the riverbank")
+        print("you saw the creature")
         print_pause()
-        print("you fight the creature and killed it !")
+        print("you fought the creature and killed it!")
         print_pause()
-        print("there is a champion ! ")
+        print("we have a champion!")
         print_pause()
-        print("your score = 1000 ")
+        print("your score = 1000")
         print_pause()
         print("you won")
         print_pause()
         try_again()
 
     elif woman == "2":
-        print(
-            "the woman refused to tell "
-            "you the place of the creature  and kicked you out of her house"
-        )
+        print("the woman refused to tell you the place of the creature and kicked you out of her house")
         print_pause()
-        print(" you have to go to the dark cave and sleep there")
+        print("you have to go to the dark cave and sleep there")
         cave()
-
 
 def game():
     intro()
@@ -145,17 +124,14 @@ def game():
     # house
     elif action == "2":
         house()
-    # invalidd
-    elif not action in ["1", "2"]:
+    # invalid
+    elif action not in ["1", "2"]:
         print("please enter a valid answer or the game will close")
         print_pause()
         action = input()
-        # cave
         if action == "1":
             cave()
-        # house
         elif action == "2":
             house()
-
 
 game()
