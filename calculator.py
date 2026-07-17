2cbe99c5556509cd34d7c4e2ae78f4bf2fbe03f6
mazzenmadly <mazenadly2008@gmail.com>
2026-07-17 23:39:42 +0300

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,35 +1,17 @@
-# first_num = input()
- 
-# sec_num = input() 
-# sign =  input() 
-
-
-
 def calculator(number_1, number_2, operator):
-    
     if operator == '+':
         result = number_1 + number_2
-   
     elif operator == '-':
         result = number_1 - number_2
-   
     elif operator == '*':
-        result = number_1 * number_2
-        
+        result = number_1 * number_2    
     elif operator == '/':
          result = number_1 / number_2
     return result
   
-
 first_num = int(input())
- 
 sec_num = int(input() )
 sign =  input() 
-
-
-# first_num = 10
-# sec_num =20
-# sign ='+'
 output = calculator(first_num, sec_num , sign)
 
 print(output)
